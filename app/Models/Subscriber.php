df2ce8f056acf67901a88813fe8ae1a69e605362
medlaq777 <m.laqsoumi@gmail.com>
2025-04-07 16:18:34 +0100

    handle the Subscriber model

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -9,4 +9,32 @@ class Subscriber extends Model
 {
     /** @use HasFactory<\Database\Factories\SubscriberFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'email',
+        'name',
+        'ip_address',
+        'user_agent',
+    ];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+    /**
+     * The attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
 }
