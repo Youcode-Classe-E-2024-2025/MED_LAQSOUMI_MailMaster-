ac713dd82da8394cf579d822f103ac69ba779a3b
medlaq777 <m.laqsoumi@gmail.com>
2025-04-08 11:29:01 +0100

    Handle the factories for newsletters

--- database/factories/NewsletterFactory.php
+++ database/factories/NewsletterFactory.php
@@ -17,7 +17,8 @@ class NewsletterFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->unique()->word(),
+            'description' => $this->faker->sentence(),
         ];
     }
 }
