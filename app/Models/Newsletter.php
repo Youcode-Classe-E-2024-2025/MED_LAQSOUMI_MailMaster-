1083e80393aa1592981bde002746a525e981f838
medlaq777 <m.laqsoumi@gmail.com>
2025-04-08 11:27:25 +0100

    Handle the model newsletters with relations needed

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Newsletter extends Model
 {
-    /** @use HasFactory<\Database\Factories\NewsletterFactory> */
     use HasFactory;
+
+    protected $fillable = ['name', 'description'];
+
+    // public function subscribers(): BelongsToMany
+    // {
+    //     return $this->belongsToMany(Subscriber::class)->withTimestamps();
+    // }
+
+    // public function campaigns(): HasMany
+    // {
+    //     return $this->hasMany(Campaign::class);
+    // }
 }
